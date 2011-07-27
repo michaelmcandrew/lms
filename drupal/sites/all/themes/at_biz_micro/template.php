8b5a25bec76c71c6c89bb1232f026683b7b5ae9b
michaelmcandrew <michaelmcandrew@thirdsectordesign.org>
2011-07-27 12:40:03 +0100

    adding a theme for the microsite (a copy of at_biz)

---
+++ drupal/sites/all/themes/at_biz_micro/template.php
@@ -0,0 +1,107 @@
+<?php
+// AT Biz
+
+/**
+ * Override or insert variables into the html template.
+ */
+function at_biz_micropreprocess_html(&$vars) {
+  drupal_add_css(path_to_theme() . '/css/ie-lte-7.css', array(
+    'group' => CSS_THEME,
+    'browsers' => array(
+      'IE' => 'lte IE 7',
+      '!IE' => FALSE,
+      ),
+    'preprocess' => FALSE,
+    )
+  );
+  drupal_add_css(path_to_theme() . '/css/ie-lte-9.css', array(
+    'group' => CSS_THEME,
+    'browsers' => array(
+      'IE' => 'lte IE 9',
+      '!IE' => FALSE,
+      ),
+    'preprocess' => FALSE,
+    )
+  );
+}
+function at_biz_microprocess_html(&$vars) {
+
+  global $theme, $theme_key;
+
+  // Hook into color module
+  if (module_exists('color')) {
+    _color_html_alter($vars);
+
+    // Mostly strait out of the color.module
+    // - get the active color scheme, or the default, or custom.
+    $info = color_get_info($theme);
+    $info['schemes'][''] = array('title' => t('Custom'), 'colors' => array());
+    $schemes = array();
+    foreach ($info['schemes'] as $key => $scheme) {
+      $schemes[$key] = $scheme['colors'];
+    }
+    $current_scheme = variable_get('color_' . $theme . '_palette', array());
+    foreach ($schemes as $key => $scheme) {
+      if ($current_scheme == $scheme) {
+        $scheme_name = $key;
+        break;
+      }
+    }
+    if (empty($scheme_name)) {
+      if (empty($current_scheme)) {
+        $scheme_name = 'default';
+      }
+      else {
+        $scheme_name = 'custom';
+      }
+    }
+  }
+
+  $classes = explode(' ', $vars['classes']);
+  if (module_exists('color')) {
+    $classes[] = 'color-scheme-' . drupal_html_class($scheme_name);
+  }
+  $classes[] = drupal_html_class($theme_key);
+  $classes[] = theme_get_setting('font_family');
+  $classes[] = theme_get_setting('headings_font_family');
+  $classes[] = theme_get_setting('font_size');
+  $classes[] = theme_get_setting('box_shadows');
+  $classes[] = theme_get_setting('body_background');
+  $classes[] = theme_get_setting('menu_bullets');
+  $classes[] = theme_get_setting('corner_radius');
+  if (theme_get_setting('headings_styles_caps') == 1) {
+    $classes[] = 'hs-caps';
+  }
+  if (theme_get_setting('headings_styles_weight') == 1) {
+    $classes[] = 'hs-fwn';
+  }
+  if (theme_get_setting('headings_styles_shadow') == 1) {
+    $classes[] = 'hs-ts';
+  }
+  $vars['classes'] = trim(implode(' ', $classes));
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function at_biz_microprocess_page(&$vars) {
+  // Hook into color.module
+  if (module_exists('color')) {
+    _color_page_alter($vars);
+  }
+}
+
+/**
+ * Override or insert variables into the block template.
+ */
+function at_biz_micropreprocess_block(&$vars) {
+  if ($vars['block']->module == 'superfish' || $vars['block']->module == 'nice_menu') {
+    $vars['content_attributes_array']['class'][] = 'clearfix';
+  }
+  if (!$vars['block']->subject) {
+    $vars['content_attributes_array']['class'][] = 'no-title';
+  }
+  if ($vars['block']->region == 'menu_bar' || $vars['block']->region == 'menu_bar_top') {
+    $vars['title_attributes_array']['class'][] = 'element-invisible';
+  }
+}
---
+++ drupal/sites/all/themes/at_biz_micro/theme-settings.php
@@ -0,0 +1,163 @@
+<?php
+// AT
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ *
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
+ */
+function at_biz_micro_form_system_theme_settings_alter(&$form, &$form_state)  {
+
+  // Create the form using Forms API: http://api.drupal.org/api/7
+  $form['layout']['equal_heights'] = array(
+    '#prefix' => '<div style="display: none">',
+    '#suffix' => '</div>',
+  );
+  $form['breadcrumb_settings']['breadcrumb']['breadcrumb_separator'] = array(
+    '#prefix' => '<div style="display: none">',
+    '#suffix' => '</div>',
+  );
+  if (theme_get_setting('enable_styles') == 'on') {
+    $form['styles'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Style settings'),
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+    );
+    $form['styles']['font'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Font and Headings settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+    );
+    $form['styles']['font']['font_family'] = array(
+      '#type' => 'select',
+      '#title' => t('Font family'),
+      '#default_value' => theme_get_setting('font_family'),
+      '#options' => array(
+        'ff-sss' => t('Helvetica Nueue, Trebuchet MS, Arial, Nimbus Sans L, FreeSans, sans-serif'),
+        'ff-ssl' => t('Verdana, Geneva, Arial, Helvetica, sans-serif'),
+        'ff-a'   => t('Arial, Helvetica, sans-serif'),
+        'ff-ss'  => t('Garamond, Perpetua, Nimbus Roman No9 L, Times New Roman, serif'),
+        'ff-sl'  => t('Baskerville, Georgia, Palatino, Palatino Linotype, Book Antiqua, URW Palladio L, serif'),
+        'ff-m'   => t('Myriad Pro, Myriad, Arial, Helvetica, sans-serif'),
+        'ff-l'   => t('Lucida Sans, Lucida Grande, Lucida Sans Unicode, Verdana, Geneva, sans-serif'),
+      ),
+    );
+    $form['styles']['font']['headings_font_family'] = array(
+      '#type' => 'select',
+      '#title' => t('Headings Font family'),
+      '#default_value' => theme_get_setting('headings_font_family'),
+      '#options' => array(
+        'hff-sss' => t('Helvetica Nueue, Trebuchet MS, Arial, Nimbus Sans L, FreeSans, sans-serif'),
+        'hff-ssl' => t('Verdana, Geneva, Arial, Helvetica, sans-serif'),
+        'hff-a'   => t('Arial, Helvetica, sans-serif'),
+        'hff-ss'  => t('Garamond, Perpetua, Nimbus Roman No9 L, Times New Roman, serif'),
+        'hff-sl'  => t('Baskerville, Georgia, Palatino, Palatino Linotype, Book Antiqua, URW Palladio L, serif'),
+        'hff-m'   => t('Myriad Pro, Myriad, Arial, Helvetica, sans-serif'),
+        'hff-l'   => t('Lucida Sans, Lucida Grande, Lucida Sans Unicode, Verdana, Geneva, sans-serif'),
+      ),
+    );
+    $form['styles']['font']['font_size'] = array(
+      '#type' => 'select',
+      '#title' => t('Base Font Size'),
+      '#default_value' => theme_get_setting('font_size'),
+      '#description' => t('This sets a base font-size on the body element - all text will scale relative to this value.'),
+      '#options' => array(
+        'fs-10' => t('0.833em'),
+        'fs-11' => t('0.917em'),
+        'fs-12' => t('1em'),
+        'fs-13' => t('1.083em'),
+        'fs-14' => t('1.167em'),
+        'fs-15' => t('1.25em'),
+        'fs-16' => t('1.333em'),
+      ),
+    );
+    $form['styles']['font']['headings_styles'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Heading Styles'),
+      '#description' => t('Add extra styles to headings. Shadows only work for CSS3 capable browsers such as Firefox, Safari, IE9 etc.'),
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+    );
+    $form['styles']['font']['headings_styles']['headings_styles_caps'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('All Caps'),
+      '#default_value' => theme_get_setting('headings_styles_caps'),
+    );
+    $form['styles']['font']['headings_styles']['headings_styles_weight'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Font weight normal'),
+      '#default_value' => theme_get_setting('headings_styles_weight'),
+    );
+    $form['styles']['font']['headings_styles']['headings_styles_shadow'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Text shadows'),
+      '#default_value' => theme_get_setting('headings_styles_shadow'),
+    );
+    $form['styles']['corners'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Rounded corner settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+    );
+    $form['styles']['corners']['corner_radius'] = array(
+      '#type' => 'select',
+      '#title' => t('Corner radius'),
+      '#default_value' => theme_get_setting('corner_radius'),
+      '#description' => t('Change the corner radius for blocks, node teasers and comments.'),
+      '#options' => array(
+        'rc-0' => t('none'),
+        'rc-4' => t('4px'),
+        'rc-8' => t('8px'),
+        'rc-12' => t('12px'),
+      ),
+    );
+    $form['styles']['pagestyles'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Page style'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+    );
+    $form['styles']['pagestyles']['body_background'] = array(
+      '#type' => 'select',
+      '#title' => t('Featured overlays'),
+      '#default_value' => theme_get_setting('body_background'),
+      '#description' => t('This setting adds a texture or pattern over the main background color.'),
+      '#options' => array(
+        'bb-n'   => t('None'),
+        'bb-b'   => t('Bubbles'),
+        'bb-hs'  => t('Horizontal stripes'),
+        'bb-dp'  => t('Diagonal pattern'),
+        'bb-dll' => t('Diagonal lines - loose'),
+        'bb-dlt' => t('Diagonal lines - tight'),
+        'bb-sd'  => t('Small dots'),
+        'bb-bd'  => t('Big dots'),
+      ),
+    );
+    $form['styles']['menu_styles'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Menu Styles'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+    );
+    $form['styles']['menu_styles']['menu_bullets'] = array(
+      '#type' => 'select',
+      '#title' => t('Menu Bullets'),
+      '#default_value' => theme_get_setting('menu_bullets'),
+      '#description' => t('Change the default menu bullets.'),
+      '#options' => array(
+        'mb-n' => t('None'),
+        'mb-dd' => t('Drupal default'),
+        'mb-ah' => t('Arrow head'),
+        'mb-ad' => t('Double arrow head'),
+        'mb-ca' => t('Circle arrow'),
+        'mb-fa' => t('Fat arrow'),
+        'mb-sa' => t('Skinny arrow'),
+      ),
+    );
+  } // endif styles
+}
